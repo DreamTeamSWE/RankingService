721e2cbec416d6dd270d36a5c1f8b8e8549e4b89
Baxxo <matteobasso9@gmail.com>
2022-03-30 21:39:51 +0200

    add class Filter.py

---
+++ Filter.py
@@ -0,0 +1,10 @@
+class Filter:
+    def __init__(self):
+        self.__zona
+        self.__orari
+        self.__tipoCucina
+        self.__punteggio
+        self.__pesiOrdinamento
+
+    def makeQuery(self) -> str:
+        return "Ciaone"
