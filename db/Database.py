7f51cf6e0297107e1da7e6537a1f4de36a5acc50
Baxxo <matteobasso9@gmail.com>
2022-04-01 15:30:46 +0200

    create db in rds

--- db/Database.py
+++ db/Database.py
@@ -1,13 +1,52 @@
+import boto3
+
+
 class Database:
-    def __init__(self):
-        self.__clusterArn = None
-        self.__secretArn = None
+    AWS_KEY = 'AKIARZPP2F6H24B5GXVA'
+    AWS_PSW = 'P61Tdsg5C4mg72PjhPULTFa9dqz0pt5hWRt+K815'
+    AWS_REGION = 'eu-central-1'
 
-    def dowritequery(self, query: str):
-        pass
+    def __init__(self, database: str) -> None:
+        self.__rdsData = boto3.client('rds-data', region_name=self.AWS_REGION, aws_access_key_id=self.AWS_KEY,
+                                      aws_secret_access_key=self.AWS_PSW)
+        self.__cluster_arn = 'arn:aws:rds:eu-central-1:123446374287:cluster:sweeat'
+        self.__secret_arn = 'arn:aws:secretsmanager:eu-central-1:123446374287:secret:rds-db-credentials/cluster' \
+                            '-AQLMTHUP2LEAFVXYXDMZFEHDR4/admin-5WXjei '
+        self.__database = database
 
-    def doreadquery(self, query: str):
-        pass
+    def __parse_result(self, results):
+        columns = [column['name'] for column in results['columnMetadata']]
+        parsed_records = []
+        for record in results['records']:
+            parsed_record = {}
+            for i, cell in enumerate(record):
+                key = columns[i]
+                value = list(cell.values())[0]
+                parsed_record[key] = value
+            parsed_records.append(parsed_record)
+        return parsed_records
 
-    def __parseresult(self, result):
-        pass
+    # param_set format: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html
+    # #RDSDataService.Client.execute_statement
+    def do_write_query(self, query: str, param_set=None):
+        if param_set is None:
+            param_set = []
+        response = self.__rdsData.execute_statement(resourceArn=self.__cluster_arn,
+                                                    secretArn=self.__secret_arn,
+                                                    database=self.__database,
+                                                    sql=query,
+                                                    parameters=param_set)
+        return response
+
+    # param_set format: https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds-data.html
+    # #RDSDataService.Client.execute_statement
+    def do_read_query(self, query: str, param_set=None):
+        if param_set is None:
+            param_set = []
+        response = self.__rdsData.execute_statement(resourceArn=self.__cluster_arn,
+                                                    secretArn=self.__secret_arn,
+                                                    database=self.__database,
+                                                    sql=query,
+                                                    parameters=param_set,
+                                                    includeResultMetadata=True)
+        return self.__parse_result(response)
---
+++ query_db/ranking_db.sql
@@ -0,0 +1,59 @@
+drop table if exists emozioni;
+create table emozioni(
+    nome_emozione varchar(50) primary key
+);
+
+drop table if exists emozioni_img;
+create table emozioni_img(
+    id_immagine integer references immagine(id_immagine),
+    nome_emozione varchar(50) references emozioni(nome_emozione),
+    qta integer
+);
+
+drop table if exists immagine;
+create table immagine(
+    id_immagine serial primary key,
+    id_post integer references post(id_post)
+);
+
+drop table if exists post;
+create table post(
+    id_post serial primary key,
+    id_ristorante integer references ristorante(id_ristorante),
+    punteggio_emoji integer,
+    sentiment_comprehend integer,
+    negative_comprehend integer,
+    positive_comprehend integer,
+    neutral_comprehend integer
+);
+
+drop table if exists ristorante;
+create table ristorante(
+    id_ristorante serial primary key,
+    nome_ristorante varchar(50),
+    indirizzo varchar(50),
+    citta varchar(50),
+    provincia varchar(50),
+    telefono varchar(50),
+    sito_web varchar(50),
+    orario_apertura varchar(50),
+    orario_chiusura varchar(50),
+    latitudine varchar(50),
+    longitudine varchar(50),
+    punteggio_emoji integer,
+    punteggio_foto integer,
+    punteggio_testo integer,
+    id_immagine integer references immagine(id_immagine)
+);
+
+drop table if exists tag;
+create table tag(
+    nome_tag varchar(50) primary key
+);
+
+drop table if exists tag_img;
+create table tag_img(
+    id_immagine integer references immagine(id_immagine),
+    nome_tag varchar(50) references tag(nome_tag),
+    qta integer
+);
