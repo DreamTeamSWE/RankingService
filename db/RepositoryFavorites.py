de476fc092714f0d49193305fd41d0a0b4afa5be
mikigatto93 <mikigatto93@gmail.com>
2022-04-11 23:47:49 +0200

    fatto gestione preferiti e modificato database

--- db/RepositoryFavorites.py
+++ db/RepositoryFavorites.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from db.DatabaseHandler import DatabaseHandler
 
 
@@ -5,14 +7,49 @@ class RepositoryFavorites:
     def __init__(self, db_name: str = 'ranking_test') -> None:
         self.database = DatabaseHandler(db_name)
 
-    def add_favorite(self, nome_utente: str, id_ristorante: int) -> None:
+    def __add_new_user(self, username: str) -> bool:
+        query = 'INSERT INTO utente (nome_utente) VALUES (:nome_utente)'
+        response = len(
+            self.database.do_write_query(
+                query, {'name': 'nome_utente', 'value': {'stringValue': username}}
+            )
+        ) > 0
+        return response
 
-        param_user_id = {'name': 'nome_utente', 'value': {'stringValue': nome_utente}}
-        param_restaurant_id = {'name': 'id_ristorante', 'value': {'longValue': id_ristorante}}
+    def __user_exists(self, username: str) -> bool:
+        query = 'SELECT * FROM utente WHERE nome_utente = :nome_utente'
+        response = len(
+            self.database.do_read_query(
+                query, {'name': 'nome_utente', 'value': {'stringValue': username}}
+            )
+        ) > 0
+        return response
+
+    def add_favorite(self, username: str, id_restaurant: int) -> bool:
+        if self.__user_exists(username):
+            print('L\'utente non esiste')
+            self.__add_new_user(username)
+            print('Aggiunto nuovo utente!')
+
+        param_user_id = {'name': 'nome_utente', 'value': {'stringValue': username}}
+        param_restaurant_id = {'name': 'id_ristorante', 'value': {'longValue': id_restaurant}}
         params = [param_user_id, param_restaurant_id]
         query = 'INSERT INTO preferiti (nome_utente, id_ristorante) VALUES (:nome_utente, :id_ristorante)'
         response = len(self.database.do_write_query(query, params)) > 0
         return response
 
+    def remove_favorite(self, username: str, id_restaurant: int) -> bool:
+        param_user_id = {'name': 'nome_utente', 'value': {'stringValue': username}}
+        param_restaurant_id = {'name': 'id_ristorante', 'value': {'longValue': id_restaurant}}
+        params = [param_user_id, param_restaurant_id]
+        query = 'DELETE FROM preferiti WHERE nome_utente = :nome_utente AND id_ristorante = :id_ristorante'
+        response = len(self.database.do_write_query(query, params)) > 0
+        return response
 
+    def get_favorites(self, username: str) -> List[str]:
+        query = 'SELECT * FROM preferiti WHERE nome_utente = :nome_utente'
+        response = self.database.do_read_query(
+                query, {'name': 'nome_utente', 'value': {'stringValue': username}}
+            )
+        return response
 
--- query_db/ranking_db.sql
+++ query_db/ranking_db.sql
@@ -2,74 +2,83 @@ ALTER DATABASE ranking_test CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci
 
 drop table if exists emozioni;
 create table emozioni(
-    nome_emozione varchar(50) primary key
+    nome_emozione varchar(50) not null primary key
 );
 
 drop table if exists emozioni_img;
 create table emozioni_img(
-    id_immagine integer references immagine(id_immagine),
-    nome_emozione varchar(50) references emozioni(nome_emozione),
-    qta integer not null default 1
+    id_immagine int not null,
+    nome_emozione varchar(50) not null,
+    qta integer not null default 1,
+    primary key (id_immagine),
+    foreign key (id_immagine) references immagine(id_immagine) on delete cascade on update cascade,
+    foreign key (nome_emozione) references emozioni(nome_emozione) on delete cascade on update cascade
 );
 
 drop table if exists immagini;
 create table immagini(
-    id_immagine integer primary key,
-    id_post integer references post(id_post)
+    id_immagine int not null,
+    id_post int not null,
+    primary key (id_immagine),
+    foreign key (id_post) references post(id_post) on delete cascade on update cascade
 );
 
 drop table if exists post;
 create table post(
-    id_post serial primary key,
+    id_post int not null,
     nome_utente varchar(50) not null,
     data_post datetime not null,
-    id_ristorante integer references ristorante(id_ristorante),
+    id_ristorante int not null,
     testo varchar(2000) not null,
-    punteggio_emoji integer,
+    punteggio_emoji int null,
     score integer not null default 0,
-    negative_comprehend integer,
-    positive_comprehend integer,
-    neutral_comprehend integer
+    negative_comprehend int null,
+    positive_comprehend int null,
+    neutral_comprehend int null,
+    primary key (id_post),
+    foreign key (id_ristorante) references ristorante(id_ristorante) on delete cascade on update cascade
 ) DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 drop table if exists ristorante;
 create table ristorante(
-    id_ristorante serial primary key,
+    id_ristorante int not null,
     nome_ristorante varchar(50) not null,
-    indirizzo varchar(50) not null,
-    telefono varchar(50) not null,
-    sito_web varchar(50) not null,
+    indirizzo varchar(50) null,
+    telefono varchar(50) null,
+    sito_web varchar(50) null,
     latitudine decimal(7,4) not null,
     longitudine decimal(7,4) not null,
     categoria varchar(100) not null,
-    punteggio_emoji integer,
-    punteggio_foto integer,
-    punteggio_testo integer
+    punteggio_emoji int null,
+    punteggio_foto int null,
+    punteggio_testo int null,
+    primary key (id_ristorante)
 );
 
 drop table if exists labels;
 create table labels(
-    nome_label varchar(50) primary key
+    nome_label varchar(50) not null primary key
 );
 
 drop table if exists labels_img;
 create table labels_img(
-    id_immagine integer references immagine(id_immagine),
-    nome_label varchar(50) references labels(nome_label),
-    qta integer
+    id_immagine int not null,
+    nome_label varchar(50) not null,
+     primary key(nome_label, id_immagine),
+    foreign key (id_immagine) references immagine(id_immagine) on delete cascade on update cascade,
+    foreign key (nome_label) references labels(nome_label) on delete cascade on update cascade
 );
 
 drop table if exists utente;
 create table utente(
-    nome_utente varchar(50) primary key,
-    email varchar(50) not null
+    nome_utente varchar(50) not null primary key
 );
 
 drop table if exists preferiti;
 create table preferiti(
     nome_utente varchar(50) not null,
-    id_ristorante integer not null,
-    primary key (nome_utente, id_ristorante),
-    foreign key (nome_utente) references utente(nome_utente),
-    foreign key (id_ristorante) references ristorante(id_ristorante)
+    id_ristorante int not null,
+    primary key (nome_utente),
+    foreign key (nome_utente) references utente(nome_utente) on delete cascade on update cascade,
+    foreign key (id_ristorante) references ristorante(id_ristorante) on delete cascade on update cascade
 );
