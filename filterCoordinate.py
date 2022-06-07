48065061a08c605fb1a48852e265d85072639b7b
matteo <matteobasso9@gmail.com>
2022-06-07 20:56:37 +0200

    add filterCoordinate.py

---
+++ filterCoordinate.py
@@ -0,0 +1,40 @@
+import json
+
+from db.RepositoryExternal import RepositoryExternal
+
+
+def get_ranking(event, context) -> json:
+    int_position = 0
+    int_size = 10
+    if 'queryStringParameters' in event and event['queryStringParameters'] is not None:
+        query_string_parameters = event['queryStringParameters']
+
+        if 'from' in query_string_parameters:
+
+
+        # check if key are presents
+        if 'from' in query_string_parameters:
+            position = query_string_parameters['from']
+            int_position = int(position)
+
+        if 'size' in query_string_parameters:
+            size = query_string_parameters['size']
+            int_size = int(size)
+
+    print("get_ranking: " + str(int_position) + " " + str(int_size))
+
+    repo_ext = RepositoryExternal()
+    list_restaurants = repo_ext.get_ranking(int_position, int_size)
+
+    response = {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Credentials': 'true',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+        },
+        "body": json.dumps(list_restaurants)
+    }
+
+    return response
